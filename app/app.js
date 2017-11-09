8b5d03ea6d42a9aa408a47bba0c85579214d9055
Kevin Valdek <kevinvaldek@gmail.com>
2017-11-09 15:44:46 +0100

    updated to work with example server.

--- app/app.js
+++ app/app.js
@@ -7,21 +7,22 @@ export default class App {
   constructor(){
 
     // Create socket sample
-    this.sampleSocket = new phoenix.Socket('wss://192.168.233.157:4443/socket');
+    this.sampleSocket = new phoenix.Socket('wss://localhost:4000/socket');
     this.sampleSocket.connect();
-    this.sampleSocket.chan = this.sampleSocket.channel('hm:api', {});
-    this.sampleSocket.chan.join()
-      .receive('ok', resp => { console.log('Joined successfully to hm:api', resp)})
-      .receive('error', resp => { console.log('Unable to join hm:api', resp) })
-
-    // You can add socket event if you needed
-    this.sampleSocket.chan.on('receive_event_sample', msg => { console.log(msg);});
-    this.sampleSocket.chan.push('send_event_sample', {});
+    
+    // You can also join any other channels:
+    // this.sampleSocket.chan = this.sampleSocket.channel('hm:api', {});
+    // this.sampleSocket.chan.join()
+    //   .receive('ok', resp => { console.log('Joined successfully to hm:api', resp)})
+    //   .receive('error', resp => { console.log('Unable to join hm:api', resp) })
+    //
+    // this.sampleSocket.chan.on('receive_event_sample', msg => { console.log(msg);});
+    // this.sampleSocket.chan.push('send_event_sample', {});
 
     // Pass endPoint or Socket Object to Adapter
     // You can also use like this
     // this.adapter = new BTWSPhoenixAdapter('wss://192.168.233.157:4443/socket');
-    this.adapter = new BTWSPhoenixAdapter(this.sampleSocket, 'ble:api');
+    this.adapter = new BTWSPhoenixAdapter(this.sampleSocket, 'bws:battery');
 
     this.adapter.on('connected', _ => document.querySelector('#state').classList.add('connected'));
     this.adapter.on('disconnected', _ => document.querySelector('#state').classList.remove('connected'));
